de80d8f47d7594ca0fac9587b9caeffef77afd7c
legoffant <legoff.ant@gmail.com>
2021-03-23 13:42:08 +0100

    commit initial

---
+++ content/hello_world.md
@@ -0,0 +1,13 @@
+Title: Hello World
+Date: 2021-03-23 11:59
+Modified: 2021-03-23 12:00
+Category: Inclassable
+Tags: Premier, Inclassable
+Authors: Anthony Le Goff
+Summary: Un premier morceau de code
+
+C'est le premier post de ce blog
+
+```python
+print("hello world")
+```
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Anthony Le Goff'
+SITENAME = 'Trivial notes'
+SITEURL = ''
+THEME = "new-bootstrap2"
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Ajout dossier d'accès aux images
+STATIC_PATHS = ['img']
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
