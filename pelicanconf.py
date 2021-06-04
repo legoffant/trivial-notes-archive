bf4036676e082fdb487e3b8f962ac994edc9c248
legoffant <legoff.ant@gmail.com>
2021-06-04 15:15:40 +0200

    sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,3 +39,20 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['pelican-plugins/']
+PLUGINS = ['sitemap']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
