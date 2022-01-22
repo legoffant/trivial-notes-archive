c37a3cf3e1b0ee72a0656f4a489238b89910cc01
legoffant <legoff.ant@gmail.com>
2022-01-22 23:22:40 +0100

    creation 2

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-
 AUTHOR = 'Anthony Le Goff'
 SITENAME = 'Trivial notes'
 SITEURL = ''
-THEME = "themes/pelican-themes/new-bootstrap2"
 
 PATH = 'content'
 
@@ -13,46 +9,23 @@ TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = 'fr'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
-RSS_FEED_SUMMARY_ONLY = False
-
-# Ajout dossier d'accès aux images
-STATIC_PATHS = ['img']
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Apprendre le C en 20H', 'https://framabook.org/le-c-en-20-heures-2/'),
-         ('Arch Linux', 'https://archlinux.org/'),
-         ('Blackarch', 'https://www.blackarch.io/'),
-         ('Kernel', 'https://www.kernel.org/'),
-         ('Arduino', 'https://www.arduino.cc/'),
-         ('BeagleBoard', 'https://beagleboard.org/bone'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/legoffant'),
-       ('Twitter', 'https://twitter.com/anth_lg'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-PLUGIN_PATHS = ['pelican-plugins/']
-PLUGINS = ['sitemap']
-
-SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
-    },
-    'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
-}
