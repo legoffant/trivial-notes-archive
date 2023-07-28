55ad1f4ba01bcc73dbf4cb2e122656ba47f2ae6a
Anthony J.R Le Goff <legoff.ant@gmail.com>
2023-07-28 02:07:16 +0200

    CC licenceV2

--- pelicanconf.py
+++ pelicanconf.py
@@ -76,9 +76,5 @@ EXTRA_PATH_METADATA = {
 }
 
 # Legal
-SITE_LICENSE = """
-&copy; Copyright 2021 by Anthony Le Goff and licensed under a <a rel="license"
-  href="http://creativecommons.org/licenses/by/4.0/">
-  <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/80x15.png" />
-  Creative Commons Attribution 4.0 International License</a>.
-"""
+
+CC_LICENSE = "CC-BY-NC"
