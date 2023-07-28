379820fc687ba5886624b67ad28b984753adfb7d
Anthony J.R Le Goff <legoff.ant@gmail.com>
2023-07-28 19:50:25 +0200

    pelicanconf.py ESP32

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,8 @@ LINKS = (('Creative Commons', 'https://creativecommons.org/'),
          ('Arduino', 'https://www.arduino.cc/'),
 	 ('MangoPi', 'https://mangopi.org/'),
 	 ('Sipeed 资料站', 'https://sipeed.com/'),
-         ('BeagleBoard', 'https://beagleboard.org/bone'),)
+         ('BeagleBoard', 'https://beagleboard.org/bone'),
+         ('Espressif ESP32', 'https://www.espressif.com/en/products/socs/esp32'),)
 
 # Social widget
 SOCIAL = (('Github', 'https://github.com/legoffant'),
